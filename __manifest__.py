c1efba057fc697adc5c48d228fed2f405d47d750
Asaduzzaman Sohel <asad@intelliservices.io>
2019-03-24 01:30:33 +0600

    learn inherit

--- __manifest__.py
+++ __manifest__.py
@@ -30,6 +30,7 @@
         #'views/templates.xml',
         'views/openacademy.xml',
         'views/session.xml',
+        'views/partner.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
