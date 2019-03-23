73636693cbe677e962cb1c3f8831972e317d5d7c
Asaduzzaman Sohel <asad@intelliservices.io>
2019-03-23 14:56:31 +0600

    added session view and session xml view

--- __manifest__.py
+++ __manifest__.py
@@ -29,6 +29,7 @@
         #'views/views.xml',
         #'views/templates.xml',
         'views/openacademy.xml',
+        'views/session.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
