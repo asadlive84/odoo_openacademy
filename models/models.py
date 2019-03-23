519bb8fb2daf49edfccf9eb74b3b4e644875dcc1
Asaduzzaman Sohel <asad@intelliservices.io>
2019-03-24 01:23:28 +0600

    added m2m

--- models/models.py
+++ models/models.py
@@ -25,7 +25,8 @@ class Course(models.Model):
 
     responsible_id=fields.Many2one('res.users', ondelete='set null', string='Responsible', index=True)
 
-    
+    session_ids=fields.One2many('openacademy.session','course_id', string="Sessions")
+
 
 
 
@@ -41,3 +42,5 @@ class Session(models.Model):
     instructor_id=fields.Many2one('res.partner', string='Instructor')
 
     course_id=fields.Many2one('openacademy.course', ondelete="cascade", string="Course", required=True)
+
+    attendee_ids=fields.Many2many('res.partner',string="Attendee")
